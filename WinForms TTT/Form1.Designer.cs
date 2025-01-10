35e12778a3e08aef126c4f9e6af4e57a35fd77eb
Neoow <142605719+HerrDextro@users.noreply.github.com>
2025-01-10 10:11:28 +0100

    Add files via upload

---
+++ WinForms TTT/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WinForms_TTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.c3 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.a2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.c3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.c2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.c1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.a3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.a1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(482, 453);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // c3
+            // 
+            this.c3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.c3.Location = new System.Drawing.Point(323, 305);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(156, 145);
+            this.c3.TabIndex = 8;
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // c2
+            // 
+            this.c2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.c2.Location = new System.Drawing.Point(163, 305);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(154, 145);
+            this.c2.TabIndex = 7;
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.c2_Click);
+            // 
+            // c1
+            // 
+            this.c1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.c1.Location = new System.Drawing.Point(3, 305);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(154, 145);
+            this.c1.TabIndex = 6;
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.c1_Click);
+            // 
+            // b3
+            // 
+            this.b3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b3.Location = new System.Drawing.Point(323, 154);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(156, 145);
+            this.b3.TabIndex = 5;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b2
+            // 
+            this.b2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b2.Location = new System.Drawing.Point(163, 154);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(154, 145);
+            this.b2.TabIndex = 4;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b1
+            // 
+            this.b1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b1.Location = new System.Drawing.Point(3, 154);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(154, 145);
+            this.b1.TabIndex = 3;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // a3
+            // 
+            this.a3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.a3.Location = new System.Drawing.Point(323, 3);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(156, 145);
+            this.a3.TabIndex = 2;
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.a3_Click);
+            // 
+            // a1
+            // 
+            this.a1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.a1.Location = new System.Drawing.Point(3, 3);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(154, 145);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.Click += new System.EventHandler(this.a1_Click);
+            // 
+            // a2
+            // 
+            this.a2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.a2.Location = new System.Drawing.Point(163, 3);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(154, 145);
+            this.a2.TabIndex = 9;
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.a2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(500, 500);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe AI";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button a2;
+    }
+}
+
---
+++ WinForms TTT/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms_TTT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void a1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void a2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void a3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void c1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void c2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void c3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
